f501cb69125a352ea181621e158ed57482f4432f
Bartosz Brudny <bartosz_magister@protonmail.com>
2026-04-10 12:17:40 +0200

    test

--- updater/PYTHON/main/logic.py
+++ updater/PYTHON/main/logic.py
@@ -113,7 +113,7 @@ def none_update(self):
 
 def open_main_app(self):
     try:
-        subprocess.Popen(['/bin/bash', self.main_path[:-7]+'/TickerK8.sh'])
+        subprocess.Popen([sys.executable, self.main_path[:-7]+'TickerK8_app/APP_FILES/PYTHON/__core__.py'])
         sys.exit(0)
     except:
         pass
