3b02864a27d976d4aa4fa8b2d8bde21f47a930cb
Bartosz-magister <129332169+Bartosz-magister@users.noreply.github.com>
2026-04-25 19:25:03 +0200

    Delete package directory

--- package/ListNews/Logic.py
+++
@@ -1,7 +0,0 @@
-#   --- Import ---
-def Setup(self):
-    #for i, button in enumerate(self.NewsList, start=1):
-    #    Button = QPushButton(self)
-    #    Button.setObjectName(f'Button{i}')
-    #    Button.setProperty('class', 'Button')
-    pass
